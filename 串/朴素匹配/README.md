083c24fb8706bea295301d96401dbcbe3f6d421d
y <459419708@qq.com>
2017-05-31 22:28:27 +0800

    字符串朴素匹配

---
+++ 串/朴素匹配/README.md
@@ -0,0 +1,5 @@
+# 字符串的朴素模式匹配
+
+也就是暴力匹配。
+
+[模式匹配算法--朴素算法](http://blog.csdn.net/allenalex/article/details/11721097)
---
+++ 串/朴素匹配/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char *str = "https://github.com/forzxy";
+	char *subStr = "github";
+
+	int strLen = strlen(str);
+	int subStrLen = strlen(subStr);
+
+	for (int i = 0; i <= strLen - subStrLen; i++) {
+		int j = 0;
+
+		for (int k = i; j < strlen(subStr); j++, k++) {
+			if (str[k] != subStr[j]) {
+				break;
+			}
+		}
+
+		if (j == strlen(subStr)) {
+			cout << "equal" << endl;
+			break;
+		}
+	}
+}
